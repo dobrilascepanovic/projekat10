dee4d63ac508d10bc90fb122051e6c8fa4d9c2cb
dobrilascepanovic <aleksandrov.dobrila@gmail.com>
2023-04-10 22:17:48 +0200

    tiket358 Ovo je krajnje resenje

--- src/test/java/swag/lab/test/checkout/LogInTest.java
+++ src/test/java/swag/lab/test/checkout/LogInTest.java
@@ -4,4 +4,15 @@ public class LogInTest {
     public void notImplementedYet10git () {
         throw new UnsupportedOperationException("TODO");
     }
+
+    public void notImplementedYet111 () {
+        throw new UnsupportedOperationException("TODO");
+    }
+    public void notImplementedYet1190 () {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public void notImplementedYet1199 () {
+        throw new UnsupportedOperationException("TODO");
+    }
 }
