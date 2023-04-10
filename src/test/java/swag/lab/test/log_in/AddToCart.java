34f76548e0d34deebd3faf8b0f1d832f3a940eb0
dobrilascepanovic <aleksandrov.dobrila@gmail.com>
2023-04-10 15:36:53 +0200

    dodata nova klasa add to cart

---
+++ src/test/java/swag/lab/test/log_in/AddToCart.java
@@ -0,0 +1,39 @@
+package swag.lab.test.log_in;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class AddToCart {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public String baseURL;
+
+    @BeforeTest
+    public void setUp()  {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        baseURL = "https://www.saucedemo.com/";
+}
+    {
+        @Test {
+        public void add to cart () throws InterruptedException {
+    }
+        {
+        }
+    }
+
+
+
+
+
+
+
