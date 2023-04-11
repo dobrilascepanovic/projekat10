aadcbf236518cff53de26d7d047eda888d6b0951
dobrilascepanovic <aleksandrov.dobrila@gmail.com>
2023-04-11 13:41:37 +0200

    tiket601 napravljene tri klase i okviru njih nove metode

---
+++ src/test/java/swag/lab/test/my_account/CheckoutAccount.java
@@ -0,0 +1,16 @@
+package swag.lab.test.my_account;
+
+public class CheckoutAccount {
+
+    public void notImplementedYet12() {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public void notImplementedYet13() {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public void notImplementedYet14() {
+        throw new UnsupportedOperationException("TODO");
+    }
+}
---
+++ src/test/java/swag/lab/test/my_account/OldAccount.java
@@ -0,0 +1,20 @@
+package swag.lab.test.my_account;
+
+public class OldAccount {
+
+    public void notImplementedYet17() {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public void notImplementedYet18() {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public void notImplementedYet19() {
+        throw new UnsupportedOperationException("TODO");
+    }
+
+    public void notImplementedYet20() {
+        throw new UnsupportedOperationException("TODO");
+    }
+}
